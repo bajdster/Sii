1edeecc262f9b960bc3eed85df79ec028129218f
Bajda <bajdster@gmail.com>
2022-01-14 00:40:03 +0100

    "zaznaczenie checkbox Lublin"

--- Pages/CareerSearch.py
+++ Pages/CareerSearch.py
@@ -13,14 +13,15 @@ class CareerSearch:
 
         #self.localizationLublinSpan = "//label[contains(@class, 'js-locationOption')]/div[contains(@class, 'sii-m-checkbox-line')]/input[@type = 'checkbox']"
 
-        self.localizationLublinSpan = "//label[@data-name ='Lublin']/div[contains(@class, 'sii-m-checkbox-line')]/input[@type = 'checkbox']"
+        #self.localizationLublinSpan = "//label[@data-name ='Lublin']/div[contains(@class, 'sii-m-checkbox-line')]/input[@type = 'checkbox']"
 
-        #self.localizationLublinSpan = "//label[@data-name ='Lublin']/div[contains(@class, 'sii-m-checkbox-line')]/span[contains(@class, 'sii-m-checkbox-line__input')]"
+        self.localizationLublinSpan = "//label[@data-name ='Lublin']/div[contains(@class, 'sii-m-checkbox-line')]/span[contains(@class, 'sii-m-checkbox-line__input')]"
 
         #self.localizationLublinSpan = "//label[@data-name ='Lublin']"
         #self.localizationLublinSpan = "//div[@class ='-col2']//span[text()='Lublin']"
         # self.localizationLublinSpan = "//span[text()='Lublin']"
-        # self.localizationLublinSpan = "//span[contains(text(),'Lublin')]"
+        #self.localizationLublinSpan = "//span[contains(text(),'Lublin')]"
+
         '''self.categories = "/html/body/section/div/div/div/div[2]/div[3]/div[3]/div/a"
         self.categoriesTesting = "//span[text()='Testing & QA']"
         self.searchIcon = "/html/body/section/div/div/div/div[2]/div[3]/a"'''
@@ -30,7 +31,12 @@ class CareerSearch:
         self.driver.find_element_by_xpath(self.workOfferSpan).click()
         self.driver.find_element_by_xpath(self.localizationXpath).click()
         #Lublin = self.driver.find_element_by_xpath(self.localizationLublinSpan)
-        self.driver.find_element_by_xpath(self.localizationLublinSpan).click()
+        print(len(self.localizationLublinSpan))
+        element = self.driver.find_element_by_xpath(self.localizationLublinSpan)
+        self.driver.execute_script("arguments[0].click();", element)
+        #^cos klika ale otweira sie strona lublin-oddzial
+
+
         #ActionChains(self.driver).move_to_element(Lublin).click(Lublin).perform()
         '''self.driver.find_element_by_xpath(self.categories).click()
         QA = self.driver.find_element_by_xpath(self.categoriesTesting).click()
